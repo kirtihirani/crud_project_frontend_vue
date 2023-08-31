c7b5a8933f1ac814117598e749247db3f1a3a20f
kirtihirani <kirtihirani89@gmail.com>
2023-08-31 22:42:35 +0530

    Adding_navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Nav_bar />
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav_bar from './components/navbar.vue';
+
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav_bar
+    // HelloWorld
   }
 }
 </script>
@@ -21,6 +26,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
