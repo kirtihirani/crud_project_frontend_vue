c5100139b3534e4b4559066e5962d3cb79292557
kirtihirani <kirtihirani89@gmail.com>
2023-09-01 14:42:58 +0530

    New Components
    Home page,AddEmployee form page

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <Nav_bar />
+  <Home />
   
 </template>
 
 <script>
 import Nav_bar from './components/navbar.vue';
-
+import Home from './components/Home.vue';
+// import AddEmployee from './components/AddEmployee.vue';
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Nav_bar
+    Nav_bar,
+    Home
+    
     // HelloWorld
   }
 }
---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="d-grid gap-2">
+  <button class="btn btn-primary" type="button">Add Employee</button>
+  <button class="btn btn-primary" type="button">Add Department</button>
+</div>
+</template>
+
+<script>
+export default {
+    name:'Home'
+}
+</script>
