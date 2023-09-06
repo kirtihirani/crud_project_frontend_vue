6845cc2aed8bc022f554acf07f02c68d8ee8628a
kirtihirani <kirtihirani89@gmail.com>
2023-09-07 01:40:43 +0530

    new branch from sqashed branch

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,23 @@
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <Nav_bar />
   <Home />
+  <AddDepartment />
   
 </template>
 
 <script>
 import Nav_bar from './components/navbar.vue';
 import Home from './components/Home.vue';
- 
+// import AddEmployee from './components/AddEmployee.vue';
+// import HelloWorld from './components/HelloWorld.vue'
+import AddDepartment from './components/AddDepartment.vue';
+
 export default {
   name: 'App',
   components: {
     Nav_bar,
     Home,
+    AddDepartment
     
   }
 }
